80f9ded3d1f5d09d84ba348368f8ebb24e211152
Eminen <eminemarque@gmail.com>
2024-10-09 17:28:57 -0300

    integrate

--- projeto-y/src/app/page.js
+++ projeto-y/src/app/page.js
@@ -2,11 +2,16 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import MenuSuperior from "./menuSuperior";
 import MenuInferior from "./menuInferior";
+import Postagem from "./postagens";
 
 export default function Home() {
   return (
     <>
       <MenuSuperior />
+      <Postagem />
+      <Postagem />
+      <Postagem />
+      <Postagem />
       <MenuInferior />
     </>
   );
