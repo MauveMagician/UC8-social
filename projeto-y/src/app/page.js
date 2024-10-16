7daf5a6ce0c5085c7d7fba6800c91f04a3d8839c
bernardo <bernardo.lima@docente.pr.senac.br>
2024-10-16 17:03:48 -0300

    fix dark mode context

--- projeto-y/src/app/page.js
+++ projeto-y/src/app/page.js
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 import MenuSuperior from "./menuSuperior";
 import MenuInferior from "./menuInferior";
 import Postagem from "./postagens";
-import DarkModeContextProvider from "../context/DarkModeContext";
+import { DarkModeContextProvider } from "./context/DarkModeContext";
 
 export default function Home() {
   return (
