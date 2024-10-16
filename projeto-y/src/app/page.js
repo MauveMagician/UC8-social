53a1def9216942f862bdec0456d406be2a305b8e
Pedro <pedroarmiliato2006@gmail.com>
2024-10-16 17:00:24 -0300

    Add svg colors

--- projeto-y/src/app/page.js
+++ projeto-y/src/app/page.js
@@ -3,19 +3,16 @@ import styles from "./page.module.css";
 import MenuSuperior from "./menuSuperior";
 import MenuInferior from "./menuInferior";
 import Postagem from "./postagens";
-import DarkModeContextProvider from "../context/DarkModeContext";
 
 export default function Home() {
   return (
     <>
-      <DarkModeContextProvider>
-        <MenuSuperior />
-        <Postagem />
-        <Postagem />
-        <Postagem />
-        <Postagem />
-        <MenuInferior />
-      </DarkModeContextProvider>
+      <MenuSuperior />
+      <Postagem />
+      <Postagem />
+      <Postagem />
+      <Postagem />
+      <MenuInferior />
     </>
   );
 }
