d2ecd73a7b687c48523512e1d9b8a9a99c5fd6da
Eminen <eminemarque@gmail.com>
2024-10-18 15:28:34 -0300

    timeline

--- projeto-y/src/app/timeline.js
+++ projeto-y/src/app/timeline.js
@@ -1,12 +1,13 @@
 import Postagem from "./postagens";
 import styles from "./timeline.module.css";
+import Conta from "./conta";
 
 export default function Timeline() {
   return (
     <>
       <div className={styles.container}>
         <Postagem />
-        <Postagem />
+        <Postagem /> <Postagem /> <Postagem />
       </div>
     </>
   );
